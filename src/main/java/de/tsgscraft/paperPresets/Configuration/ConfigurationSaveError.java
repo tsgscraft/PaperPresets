e172fe762ba9952db88471c5626d26fa9caf3c77
tsgscraft <100886182+tsgscraft@users.noreply.github.com>
2025-05-15 09:50:38 +0200

    Delete src/main directory

--- src/main/java/de/tsgscraft/paperPresets/Configuration/ConfigurationSaveError.java
+++
@@ -1,9 +0,0 @@
-package de.tsgscraft.paperPresets.Configuration;
-
-import java.io.IOException;
-
-public class ConfigurationSaveError extends RuntimeException {
-    public ConfigurationSaveError(String config, IOException e) {
-        super("Failed to save Config: " + config, e);
-    }
-}
